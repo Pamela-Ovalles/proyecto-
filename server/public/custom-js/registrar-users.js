6e3215ef2a17e4a53b4dfecd73581bf05b230610
Pamela-Ovalles <51461846+Pamela-Ovalles@users.noreply.github.com>
2019-08-12 19:11:18 -0400

    funcion para registar usuario

--- server/public/custom-js/registrar-users.js
+++ server/public/custom-js/registrar-users.js
@@ -1,1 +1,41 @@
 
+
+async function registrar_users(req,res){
+  const {primernombre,segundonombre,primerapellido,segundoapellido,cedula,nacionalidad,
+	fechanacimiento,sexo,correo,clave,confirpass,telefonousuario,provincia,municipio,ciudad,
+  sector,calle} = req.body;
+
+  try {
+
+    const newUser = await  newUser.create({
+  		primernombre,
+  		segundonombre,
+  		primerapellido,
+  		segundoapellido,
+  		cedula,nacionalidad,
+  		fechanacimiento,
+  		sexo,
+  		correo,
+  		clave,
+  		confirpass,
+  		telefonousuario,
+  		provincia,municipio,
+  		ciudad,
+  	  sector,
+  		calle
+      });
+      if(newUser){
+        res.json({
+          message: 'Usuario creado correctamente',
+          data: newUser
+        });
+      }
+
+  } catch (e) {
+    console.error(newUser);
+    res.status(500).json({
+      message: 'hubo un error',
+      data: {}
+    });
+  }
+}
